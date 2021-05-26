5ec1fe888baf64888d07db458e3196208a7eddff
dazzlemon <daanill13470@gmail.com>
2021-05-26 21:06:44 +0300

    started polishing

--- src/bot.py
+++ src/bot.py
@@ -3,36 +3,58 @@ TELEGRAM BOT
 """
 
 import logging
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackQueryHandler
+from typing       import Callable
+from dataclasses  import dataclass
+from telegram.ext import (Updater,
+                         CommandHandler,
+                         MessageHandler,
+                         Filters,
+                         CallbackQueryHandler)
+from user         import User
 
 class Bot:
     """
     TELEGRAM BOT
     """
-    def __init__(self, token, commands, default_handler=None, button_handler=None):
-        self._updater = Updater(
-            token=token,
-            use_context=True
-        )
+    def __init__(
+        self, token, commands, default_handler=None, button_handler=None
+    ):
+        self._updater = Updater(token=token, use_context=True)
         self._dispatcher = self._updater.dispatcher
-        self._commands = commands
 
-        for command in commands:
-            handler = CommandHandler(command.keyword, command.handler)
-            self._dispatcher.add_handler(handler)
+        self._commands = commands
+        self._init_commands()
 
         self._default_handler = default_handler
-        if not default_handler is None:
+        self._init_default_handler()
+
+        self._init_button_handler(button_handler)
+        self._init_logging()
+
+
+    def _init_default_handler(self):
+        if self._default_handler is not None:
             handler = MessageHandler(
                 Filters.text & (~Filters.command),
-                default_handler
+                self._default_handler
             )
             self._dispatcher.add_handler(handler)
 
+
+    def _init_commands(self):
+        for command in self._commands:
+            handler = CommandHandler(command.keyword, command.handler)
+            self._dispatcher.add_handler(handler)
+
+
+    def _init_button_handler(self, button_handler):
         if not button_handler is None:
             handler = CallbackQueryHandler(button_handler)
             self._dispatcher.add_handler(handler)
 
+
+    @staticmethod
+    def _init_logging():
         logging.basicConfig(
             format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
             level=logging.DEBUG
@@ -43,10 +65,11 @@ class Bot:
         """START UPDATING BOT"""
         self._updater.start_polling()
 
+
+@dataclass
 class Command:
     """TELEGRAM BOT COMMAND HANDLER"""
-    def __init__(self, keyword, handler, description):
-        self.keyword = keyword
-        self.handler = handler
-        self.description = description
+    keyword: str
+    handler: Callable[[User], str]
+    description: str
         
