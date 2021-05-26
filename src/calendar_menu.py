a3d8513c3a4c72868f0e67fa7ec13231be762816
dazzlemon <daanill13470@gmail.com>
2021-05-26 23:54:08 +0300

    CalendarMenu now extends MultiPageMenu

--- src/calendar_menu.py
+++ src/calendar_menu.py
@@ -3,14 +3,14 @@ Menu to choose calendar day
 """
 
 import calendar
-from datetime import date
-from typing   import List
-from telegram import Message, InlineKeyboardMarkup, InlineKeyboardButton
-from button   import Button
-from user     import User
+from datetime       import date
+from typing         import List
+from telegram       import Message, InlineKeyboardMarkup, InlineKeyboardButton
+from user           import User
+from multipage_menu import MultiPageMenu
 
 
-class CalendarMenu(Button):
+class CalendarMenu(MultiPageMenu):
     """
     MENU TO CHOOSE CALENDAR DAY, callback is <CALENDAR_DAY=YYYY/MM/DD>
     """
@@ -19,23 +19,30 @@ class CalendarMenu(Button):
     ):
         self.start = start
         self.finish = finish
-        self.has_parent = has_parent
+        self._has_parent = has_parent
         self.callback = callback
 
         self.current_page = 0
 
 
-    def callback_args(self) -> str:
-        return ''
+    @property
+    def has_parent(self) -> bool:
+        return self._has_parent
 
 
-    def operation(self, message: Message, command: str, user: User):
-        keyboard = self.keyboard()
-        self._add_page_nav_buttons(keyboard)
-        self._add_nav_buttons(keyboard)
+    def max_page(self) -> int:
+        return ((self.finish.year - self.start.year) * 12
+             + (self.finish.month - self.start.month))
 
-        markup = InlineKeyboardMarkup(keyboard)
-        message.edit_reply_markup(reply_markup=markup)
+
+    @property
+    def current_page(self) -> int:
+        return self._current_page
+
+
+    @current_page.setter
+    def current_page(self, val: int):
+        self._current_page = val
 
 
     def keyboard(self) -> List[List[InlineKeyboardButton]]:
@@ -71,7 +78,7 @@ class CalendarMenu(Button):
             row = []
             for day in week:
                 if day == 0:
-                    row.append(InlineKeyboardButton(" ", callback_data='pass'))
+                    row.append(self.empty_button())
                 else:
                     row.append(InlineKeyboardButton(
                         day,
@@ -80,45 +87,3 @@ class CalendarMenu(Button):
                         ) + ';' + self.callback
                     ))
             keyboard.append(row)
-
-
-    def _add_nav_buttons(self, keyboard):
-        if self.has_parent:
-            keyboard.append(
-                [InlineKeyboardButton('Back', callback_data='back')]
-            )
-        keyboard.append(
-            [InlineKeyboardButton('Exit', callback_data='exit')]
-        )
-
-
-    def _add_page_nav_buttons(self, keyboard):
-        keyboard.append([self._prev_button(), self._next_button()])
-
-
-    def _next_button(self):
-        diff = ((self.finish.year - self.start.year) * 12
-             + (self.finish.month - self.start.month))
-        if self.current_page + 1 < diff:
-            return InlineKeyboardButton(
-                '>',
-                callback_data='next_page'
-            )
-        else:
-            return self.empty_button()
-
-
-    def _prev_button(self):
-        if self.current_page > 0:
-            return InlineKeyboardButton(
-                '<',
-                callback_data='prev_page'
-            )
-        else:
-            return self.empty_button()
-
-
-    @staticmethod
-    def empty_button():
-        """returns button with no text and 'pass' callback"""
-        return InlineKeyboardButton(' ', callback_data='pass')
